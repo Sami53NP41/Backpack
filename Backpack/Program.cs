8ae50c48743166b93ea767a6ac349f73d02e4208
Sami53NP41 <samuil.stanchev@pgmett.com>
2023-10-30 10:25:20 +0200

    Add project files.

---
+++ Backpack/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void BackPackAlgorithm(int BPparameter, int itemsWeight, int itemsValue)
+    {
+        
+        
+
+        
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("Type BackPack parameter:");
+        int BPparameter = 0;
+        Console.WriteLine("Type items weight:");
+        int[] itemsWeight = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        Console.WriteLine("Type items value:");
+        int[] itemsValue = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    }
+}
